1f74b33562e49d97aacb5ca2bc01715b7eeacff0
sungjonghwa <sjh@grida.dev>
2024-04-04 14:27:33 +0900

    feat : 빌드 후 푸시

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h4>나는 김시율이고 바보입니다.</h4>
       <div>하지만 갱신하지 않는 블로그 주소정도는 있어요</div>
       <div>알려드릴까요?</div>
+      <br />
       <a href="https://m.blog.naver.com/octlolo?fbclid=PAAaZy1fW4qxbt3j7TajpKOTSRtvmeMpkBaa_HhOBgsERsK_GhouXg6y1Tmjk_aem_AXHfwpVmrQv841DEapvRUUS3MchNTxMnHEH_qSjR9SrSd0gJqZ0E58fkEaSS6dBUnXM&tab=1">
         시율's 블로그
       </a>
