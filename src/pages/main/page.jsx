fec05963c6591a5b38be73594fa35124abb340fb
sungjonghwa <sjh@grida.dev>
2024-07-25 13:54:57 +0900

    feat : 페이지 방식 수정

--- src/pages/main/page.jsx
+++ src/pages/main/page.jsx
@@ -1,11 +1,10 @@
-import React, { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
-import { useNavigate } from "react-router-dom";
+import React, { useRef, useState, useEffect } from "react";
+import { useFrame, Canvas, useThree } from "@react-three/fiber";
 import { useSpring, animated } from "react-spring";
 import styled from "styled-components";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
+import { OrbitControls, Stars, Text3D } from "@react-three/drei";
 import { Link as ScrollLink, Element as ScrollElement } from "react-scroll";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
 const skills = [
   "JavaScript",
@@ -28,14 +27,19 @@ const skills = [
 ];
 
 const MainPage = () => {
-  const [showIntro, setShowIntro] = React.useState(true);
+  const [showIntro, setShowIntro] = useState(true);
+  const [showMainContent, setShowMainContent] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const timer1 = setTimeout(() => setShowIntro(false), 5000);
     return () => {
       clearTimeout(timer1);
     };
   }, []);
+
+  const handleClick = () => {
+    setShowMainContent(true);
+  };
 
   const half = Math.ceil(skills.length / 2);
   const firstHalf = skills.slice(0, half);
@@ -49,126 +53,155 @@ const MainPage = () => {
           <FadeText delay={2000}>성종화입니다.</FadeText>
         </IntroContainer>
       ) : (
-        <MainContainer>
-          <CanvasContainer>
-            <Canvas>
-              <ambientLight intensity={0.5} />
-              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-              <pointLight position={[-10, -10, -10]} />
-              <Stars />
-              <OrbitControls />
-              <Laptop />
-            </Canvas>
-          </CanvasContainer>
-          <ScrollLinkContainer>
-            <ScrollLink to="section1" smooth={true} duration={1000}>
-              소개
-            </ScrollLink>
-            <ScrollLink to="section2" smooth={true} duration={1000}>
-              프로젝트
-            </ScrollLink>
-            <ScrollLink to="section3" smooth={true} duration={1000}>
-              연락처
-            </ScrollLink>
-            <ScrollLink to="skills" smooth={true} duration={1000}>
-              스킬
-            </ScrollLink>
-          </ScrollLinkContainer>
-          <Sections>
-            <ScrollElement name="section1">
-              <Section>
-                <h2>소개</h2>
-                <p>여기는 소개 섹션입니다.</p>
-              </Section>
-            </ScrollElement>
-
-            <ScrollElement name="skills">
-              <SkillsSection>
-                <h2>스킬</h2>
-                <SkillsWrapper>
-                  <SkillsSlider>
-                    <SkillRow>
-                      {firstHalf.map((skill, index) => (
-                        <SkillItem key={index}>{skill}</SkillItem>
-                      ))}
-                      {firstHalf.map((skill, index) => (
-                        <SkillItem key={index + firstHalf.length}>
-                          {skill}
-                        </SkillItem>
-                      ))}
-                    </SkillRow>
-                    <SkillRow reverse>
-                      {secondHalf.map((skill, index) => (
-                        <SkillItem key={index}>{skill}</SkillItem>
-                      ))}
-                      {secondHalf.map((skill, index) => (
-                        <SkillItem key={index + secondHalf.length}>
-                          {skill}
-                        </SkillItem>
-                      ))}
-                    </SkillRow>
-                  </SkillsSlider>
-                </SkillsWrapper>
-              </SkillsSection>
-            </ScrollElement>
-
-            <ScrollElement name="section2">
-              <Section>
-                <h2>프로젝트</h2>
-                <p>여기는 프로젝트 섹션입니다.</p>
-              </Section>
-            </ScrollElement>
-
-            <ScrollElement name="section3">
-              <Section>
-                <h2>연락처</h2>
-                <p>여기는 연락처 섹션입니다.</p>
-              </Section>
-            </ScrollElement>
-          </Sections>
-        </MainContainer>
+        <>
+          {!showMainContent ? (
+            <CanvasContainer>
+              <Canvas camera={{ position: [0, 0, 300] }}>
+                <AnimatedCamera />
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                <pointLight position={[-10, -10, -10]} />
+                <Stars />
+                <OrbitControls />
+                <MovingText handleClick={handleClick} />
+                <EffectComposer>
+                  <Bloom
+                    luminanceThreshold={0.1}
+                    luminanceSmoothing={0.9}
+                    height={300}
+                  />
+                </EffectComposer>
+              </Canvas>
+            </CanvasContainer>
+          ) : (
+            <MainContainer>
+              <ScrollLinkContainer>
+                <StyledScrollLink to="section1" smooth={true} duration={1000}>
+                  소개
+                </StyledScrollLink>
+                <StyledScrollLink to="section2" smooth={true} duration={1000}>
+                  프로젝트
+                </StyledScrollLink>
+                <StyledScrollLink to="skills" smooth={true} duration={1000}>
+                  스킬
+                </StyledScrollLink>
+              </ScrollLinkContainer>
+              <Sections>
+                <ScrollElement name="section1">
+                  <Section>
+                    <SectionTitle>소개</SectionTitle>
+                    <SectionContent>여기는 소개 섹션입니다.</SectionContent>
+                  </Section>
+                </ScrollElement>
+                <ScrollElement name="skills">
+                  <SkillsSection>
+                    <SectionTitle>스킬</SectionTitle>
+                    <SkillsWrapper>
+                      <SkillsSlider>
+                        <SkillRow>
+                          {firstHalf.map((skill, index) => (
+                            <SkillItem key={index}>{skill}</SkillItem>
+                          ))}
+                          {firstHalf.map((skill, index) => (
+                            <SkillItem key={index + firstHalf.length}>
+                              {skill}
+                            </SkillItem>
+                          ))}
+                        </SkillRow>
+                        <SkillRow>
+                          {secondHalf.map((skill, index) => (
+                            <SkillItem key={index}>{skill}</SkillItem>
+                          ))}
+                          {secondHalf.map((skill, index) => (
+                            <SkillItem key={index + secondHalf.length}>
+                              {skill}
+                            </SkillItem>
+                          ))}
+                        </SkillRow>
+                      </SkillsSlider>
+                    </SkillsWrapper>
+                  </SkillsSection>
+                </ScrollElement>
+                <ScrollElement name="section2">
+                  <Section>
+                    <SectionTitle>프로젝트</SectionTitle>
+                    <SectionContent>여기는 프로젝트 섹션입니다.</SectionContent>
+                  </Section>
+                </ScrollElement>
+              </Sections>
+            </MainContainer>
+          )}
+        </>
       )}
     </>
   );
 };
 
-export default MainPage;
-
-const Laptop = () => {
+const MovingText = ({ handleClick }) => {
   const ref = useRef();
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  const speed = 0.1;
+  let time = 0;
 
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate("/home");
-  };
+  useFrame(() => {
+    time += speed;
+    ref.current.position.y = Math.sin(time) * 0.5;
+  });
 
   return (
-    <group ref={ref} position={[0, 0, 0]} onClick={handleClick}>
-      <mesh position={[0, -0.5, 0]}>
-        <boxGeometry args={[1.5, 0.1, 1]} />
-        <meshStandardMaterial color={"silver"} />
+    <>
+      <Text3D
+        ref={ref}
+        position={[-1, 0, 0]}
+        rotation={[0, 0, 0]}
+        size={0.7}
+        height={0.3}
+        curveSegments={32}
+        bevelEnabled
+        bevelThickness={0.05}
+        bevelSize={0.03}
+        bevelOffset={0}
+        bevelSegments={8}
+        font="/fonts/helvetiker_bold.typeface.json"
+        onClick={handleClick}
+      >
+        Click
+        <meshStandardMaterial
+          attach="material"
+          color="lightblue"
+          emissive="#00adee"
+          emissiveIntensity={0.8}
+          roughness={0.4}
+          metalness={0.5}
+        />
+      </Text3D>
+      {/* <mesh position={[1, -0.5, 0]}>
+        <boxGeometry args={[0.7, 0.4, 0.1]} />
+        <meshStandardMaterial color="gray" />
       </mesh>
-      <mesh position={[0, 0.3, -0.5]} rotation={[1.57, 0, 0]}>
-        <boxGeometry args={[1.5, 0.05, 1]} />
-        <meshStandardMaterial color={"#1b1b1b"} />
-      </mesh>
-      <mesh position={[0, 0.35, -0.45]}>
-        <planeGeometry args={[1.4, 0.8]} />
-        <meshStandardMaterial color={"#000000"} />
-      </mesh>
-      <mesh position={[0.65, 0.32, -0.48]}>
-        <boxGeometry args={[0.02, 0.02, 0.02]} />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
-      <mesh position={[-0.65, 0.32, -0.48]}>
-        <boxGeometry args={[0.02, 0.02, 0.02]} />
-        <meshStandardMaterial color={"green"} />
-      </mesh>
-    </group>
+      <mesh position={[1, -0.3, 0.1]}>
+        <planeGeometry args={[0.6, 0.3]} />
+        <meshStandardMaterial color="black" />
+      </mesh> */}
+    </>
   );
 };
+
+const AnimatedCamera = () => {
+  const { camera } = useThree();
+  const spring = useSpring({
+    from: { z: 300 },
+    to: { z: 10 },
+    config: { duration: 1000 },
+  });
+
+  useFrame(() => {
+    camera.position.z = spring.z.get();
+  });
+
+  return null;
+};
+
+export default MainPage;
 
 const IntroContainer = styled.div`
   display: flex;
@@ -183,13 +216,14 @@ const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: black;
+  background-color: #121212;
   color: white;
 `;
 
 const CanvasContainer = styled.div`
   width: 100%;
-  height: 50vh;
+  height: 100vh;
+  cursor: pointer;
 `;
 
 const ScrollLinkContainer = styled.div`
@@ -197,7 +231,20 @@ const ScrollLinkContainer = styled.div`
   justify-content: space-around;
   width: 100%;
   background-color: #222;
-  padding: 10px 0;
+  padding: 20px 0;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+`;
+
+const StyledScrollLink = styled(ScrollLink)`
+  color: #ffffff;
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  transition: color 0.3s;
+
+  &:hover {
+    color: #00adee;
+  }
 `;
 
 const Sections = styled.div`
@@ -205,14 +252,34 @@ const Sections = styled.div`
 `;
 
 const Section = styled.div`
-  padding: 50px;
+  padding: 60px 20px;
   text-align: center;
+  background-color: #1e1e1e;
+  margin: 20px 0;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 2.5rem;
+  margin-bottom: 20px;
+  color: #00adee;
+`;
+
+const SectionContent = styled.p`
+  font-size: 1.2rem;
+  color: #d4d4d4;
 `;
 
 const AnimatedText = styled(animated.div)`
   color: #ffffff;
   font-size: 4rem;
   position: absolute;
+  font-family: "Arial", sans-serif;
+
+  @media (max-width: 1024px) {
+    font-size: 2rem;
+  }
 `;
 
 const FadeText = ({ children, delay = 0 }) => {
@@ -223,7 +290,7 @@ const FadeText = ({ children, delay = 0 }) => {
       await next({ opacity: 0, transform: "scale(1.5)" });
     },
     config: { duration: 1000 },
-    delay: delay,
+    delay,
     reset: true,
   });
 
@@ -231,7 +298,7 @@ const FadeText = ({ children, delay = 0 }) => {
 };
 
 const SkillsSection = styled.div`
-  padding: 50px;
+  padding: 60px 20px;
   text-align: center;
   color: #ffffff;
   margin-bottom: 20px;
@@ -255,7 +322,7 @@ const SkillRow = styled.div`
   display: flex;
   flex-wrap: nowrap;
   white-space: nowrap;
-  animation: ${({ reverse }) => (reverse ? "scroll-reverse" : "scroll")} 60s
+  animation: ${({ reverse }) => (reverse ? "scroll-reverse" : "scroll")} 10s
     linear infinite;
 
   @keyframes scroll {
@@ -283,4 +350,10 @@ const SkillItem = styled.div`
   font-size: 1.5rem;
   font-weight: bold;
   color: #ffffff;
+  transition: transform 0.3s;
+
+  &:hover {
+    transform: scale(1.1);
+    color: #00adee;
+  }
 `;
